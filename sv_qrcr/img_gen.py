25e25d3a3cb27ba8c0887f723b8623590d24039a
夏莫 <2838596569@qq.com>
2023-06-16 21:43:01 +0800

    调整字间距

--- sv_qrcr/img_gen.py
+++ sv_qrcr/img_gen.py
@@ -6,6 +6,44 @@ import base64
 
 font = ImageFont.truetype(join(MOUDULE_PATH,'font/font.ttf'),size = 15)
 
+def draw_text_psd_style(draw, xy, text, font, tracking=0, leading=None, **kwargs):
+    """
+    usage: draw_text_psd_style(draw, (0, 0), "Test", 
+                tracking=-0.1, leading=32, fill="Blue")
+
+    Leading is measured from the baseline of one line of text to the
+    baseline of the line above it. Baseline is the invisible line on which most
+    letters—that is, those without descenders—sit. The default auto-leading
+    option sets the leading at 120% of the type size (for example, 12‑point
+    leading for 10‑point type).
+
+    Tracking is measured in 1/1000 em, a unit of measure that is relative to 
+    the current type size. In a 6 point font, 1 em equals 6 points; 
+    in a 10 point font, 1 em equals 10 points. Tracking
+    is strictly proportional to the current type size.
+    """
+    def stutter_chunk(lst, size, overlap=0, default=None):
+        for i in range(0, len(lst), size - overlap):
+            r = list(lst[i:i + size])
+            while len(r) < size:
+                r.append(default)
+            yield r
+    x, y = xy
+    font_size = font.size
+    lines = text.splitlines()
+    if leading is None:
+        leading = font.size * 1.2
+    for line in lines:
+        for a, b in stutter_chunk(line, 2, 1, ' '):
+            w = font.getlength(a + b) - font.getlength(b)
+            # dprint("[debug] kwargs")
+            print("[debug] kwargs:{}".format(kwargs))
+                
+            draw.text((x, y), a, font=font, **kwargs)
+            x += w + (tracking / 1000) * font_size
+        y += leading
+        x = xy[0]
+
 async def deck_img_gen(deck:dict)->str:
     """
     通过卡组列表生成卡组图片
@@ -37,7 +75,8 @@ async def deck_img_gen(deck:dict)->str:
                 img.paste(lcard,((rol-1)*270,(row-1)*46))
                 costimg = Image.open(join(MOUDULE_PATH,f'img/cost/{cards[i]["cost"]}.png'))
                 img.paste(costimg,((rol-1)*270+10,(row-1)*46+10),costimg)
-                draw.text(((rol-1)*270+50,(row-1)*46+23),cards[i]['name'],(255,255,255),font,'lm')
+                y = font.getsize(cards[i]['name'])[1]
+                draw_text_psd_style(draw,((rol-1)*270+50,int((row-1)*46+23-y/2)),cards[i]['name'],font,-2)
                 draw.text(((rol)*270-10,(row-1)*46+23),f'x{cards[i]["num"]}',(255,255,255),font,'rm')
                 count +=1
     img = img.convert('RGB')
