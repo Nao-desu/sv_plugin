c97b2e042e41caecfb363a3bf2a2a1d09357f101
夏莫 <2838596569@qq.com>
2023-09-25 10:58:07 +0800

    fix(终于写对了真是不容易)

--- sv_games/sv_voice_gess.py
+++ sv_games/sv_voice_gess.py
@@ -2,13 +2,13 @@ from ..info import clan2w
 from ..config import GAME_TIME
 from ..info import MOUDULE_PATH
 from os.path import join
+from nonebot import MessageSegment
 import random,os
 
 async def guess_voice(bot,ev,limited,clan,answer):
     w1 = '指定' if limited else ''
     w2 = '' if not clan else clan2w[clan]
     voice = random.choice(os.listdir(join(MOUDULE_PATH,f"voice\\{answer}")))
-    voice_path = join(MOUDULE_PATH,f"voive\\{answer}\\{voice}")
-    rec = f'[CQ:record,file=file:///{os.path.abspath(voice_path)}]'
+    rec = MessageSegment.record(f'file:///{MOUDULE_PATH}/voice/{answer}/{voice}')
     await bot.send(ev,f'猜猜这段语音来自哪张{w1}{w2}卡牌?\n{GAME_TIME}秒后公布答案')
     await bot.send(ev,rec)
