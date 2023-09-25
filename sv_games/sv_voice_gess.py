76f9426a52193f6192d9888f6dbc50f9e078a0a0
夏莫 <2838596569@qq.com>
2023-09-25 10:31:17 +0800

    fix

--- sv_games/sv_voice_gess.py
+++ sv_games/sv_voice_gess.py
@@ -8,7 +8,7 @@ async def guess_voice(bot,ev,limited,clan,answer):
     w1 = '指定' if limited else ''
     w2 = '' if not clan else clan2w[clan]
     voice = random.choice(os.listdir(join(MOUDULE_PATH,f"voice\\{answer}")))
-    voice_path = join(MOUDULE_PATH,f'voive\\{voice}')
+    voice_path = join(MOUDULE_PATH,f'voive\\{answer}\\{voice}')
     rec = f'[CQ:record,file=file:///{os.path.abspath(voice_path)}]'
     await bot.send(ev,f'猜猜这段语音来自哪张{w1}{w2}卡牌?\n{GAME_TIME}秒后公布答案')
     await bot.send(ev,rec)
