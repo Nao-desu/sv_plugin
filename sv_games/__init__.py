8ab83a8170b321c6162a5ff5edc62b6d07a1062c
夏莫 <2838596569@qq.com>
2023-09-26 09:31:59 +0800

    改变游戏结束判定时点/图片防吞/重置游戏指令

--- sv_games/__init__.py
+++ sv_games/__init__.py
@@ -5,7 +5,10 @@ from os.path import join,abspath
 from .sv_voice_gess import guess_voice
 from .sv_paint_guess import guess_paint
 from ..config import GAME_TIME
-import asyncio,zhconv
+from PIL import Image
+from io import BytesIO
+import asyncio,zhconv,base64
+import numpy as np
 
 sv_help = """
 [sv猜卡面] 猜猜bot随机发送的卡面的一小部分来自哪张影之诗卡牌
@@ -65,13 +68,13 @@ async def voice_guess(bot,ev):
             if gm.get_ans(gid) != answer:
                 return
             img_path = join(MOUDULE_PATH,f"img\\full\\{answer}0.png")
-            img = f'[CQ:image,file=file:///{abspath(img_path)}]'
+            img = change_img(img_path)
+            gm.end_game(gid)
             await bot.send(ev, f"正确答案是:{get_cards()[str(answer)]['card_name']}\n{img}\n很遗憾,没有人答对")
-        gm.end_game(gid)
         return
     except Exception as e:
-        await bot.send(ev,f'游戏发生错误，自动终止')
         gm.end_game(gid)
+        await bot.send(ev,f'游戏发生错误，自动终止\n{e}')
 
 @sv.on_prefix('sv猜卡面')
 async def paint_guess(bot,ev):
@@ -90,13 +93,13 @@ async def paint_guess(bot,ev):
             if gm.get_ans(gid) != answer:
                 return
             img_path = join(MOUDULE_PATH,f"img\\full\\{answer}0.png")
-            img = f'[CQ:image,file=file:///{abspath(img_path)}]'
+            img = change_img(img_path)
+            gm.end_game(gid)
             await bot.send(ev, f"正确答案是:{get_cards()[str(answer)]['card_name']}\n{img}\n很遗憾,没有人答对")
-        gm.end_game(gid)
         return
     except Exception as e:
-        await bot.send(ev,f'游戏发生错误，自动终止')
         gm.end_game(gid)
+        await bot.send(ev,f'游戏发生错误，自动终止\n{e}')
 
 @sv.on_message()
 async def on_input_chara_name(bot, ev):
@@ -107,6 +110,35 @@ async def on_input_chara_name(bot, ev):
     if gm.check_ans(gid,zhconv.convert(ev.message.extract_plain_text(),'zh-tw')):
         gm.end_game(gid)
         img_path = join(MOUDULE_PATH,f"img\\full\\{answer}0.png")
-        img = f'[CQ:image,file=file:///{abspath(img_path)}]'
+        img = change_img(img_path)
         msg = f"正确答案是:{get_cards()[str(answer)]['card_name']}\n{img}\n{Seg.at(ev.user_id)}猜对了，真厉害！"
         await bot.send(ev, msg)
+
+@sv.on_fullmatch('重置游戏')
+async def reset_games(bot,ev):
+    gm.end_game(ev.group_id)
+    await bot.send(ev,'已重置')
+
+async def change_img(path:str):
+    """
+    图片防吞
+    """
+    # 读取图片并转换为numpy数组
+    img = Image.open("example.jpg")
+    img_arr = np.array(img)
+
+    # 生成一个随机的行索引和列索引
+    row = np.random.randint(0, img_arr.shape[0])
+    col = np.random.randint(0, img_arr.shape[1])
+
+    # 生成一个随机的颜色值
+    color = np.random.randint(0, 256, 3)
+
+    # 将图片数组中对应位置的像素值替换为随机颜色值
+    img_arr[row, col] = color
+    new_img = Image.fromarray(img_arr)
+    region = new_img.convert('RGB')
+    buf = BytesIO()
+    region.save(buf, format='JPEG')
+    base64_str = f'base64://{base64.b64encode(buf.getvalue()).decode()}'
+    return f'[CQ:image,file={base64_str}]'
