231f31d8a1fdfa81530e92a2ae52d0304b3d8444
夏莫 <2838596569@qq.com>
2023-12-09 21:06:28 +0800

    fix

--- sv_master/__init__.py
+++ sv_master/__init__.py
@@ -5,6 +5,6 @@ sv = Service('sv_master')
 
 @sv.on_prefix('Ratings')
 async def ratings_info(bot,ev):
-    msg1,msg2 = get_ratings_data()
+    msg1,msg2 = await get_ratings_data()
     await bot.send(ev,msg1)
     await bot.send(ev,msg2)
