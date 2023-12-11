46301a13f095d2cbca554d0f2d18301c30fe629f
夏莫 <2838596569@qq.com>
2023-12-11 16:16:49 +0800

    改半天发现取错函数了

--- sv_master/decks.py
+++ sv_master/decks.py
@@ -1,12 +1,12 @@
 from ..info import find_decks
-from .img_gen import deck_img_gen
+from .img_gen import deck_img
 from io import BytesIO
 import random,base64
 
 async def get_deck_data(text):
     decks = await find_decks(text)
     if decks:
-        img = await deck_img_gen(random.choice(decks))
+        img = await deck_img(random.choice(decks))
         buf = BytesIO()
         img.save(buf, format='JPEG')
         base64_str = f'base64://{base64.b64encode(buf.getvalue()).decode()}'
