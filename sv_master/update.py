9e7298eae430ebec7819c39ca0c7253c7b845a48
夏莫 <2838596569@qq.com>
2024-06-09 23:04:18 +0800

    数据模块：移除ratings

--- sv_master/update.py
+++ sv_master/update.py
@@ -6,7 +6,6 @@ from httpx import AsyncClient
 from os.path import join
 from ..info import MOUDULE_PATH,hashToID,get_deck_name
 
-rating_url = "https://shadowversemaster.com/ratings/__data.json?x-sveltekit-invalidated=01"
 deck_url = "https://shadowversemaster.com/archetype/"
 deckdata_r = []
 deckdata_l = []
@@ -100,34 +99,8 @@ async def deck_update():
     deckname_num_l = {}
     print("卡组数据更新完毕")
 
-async def rating_update():
-    print("开始更新Ratings")
-    async with AsyncClient() as client:
-        while(1):
-            try:
-                req = await client.get(url=rating_url,timeout=None)
-                break
-            except:
-                pass
-    d = json.loads(req.text)['nodes'][1]['data']
-    realdata = {}
-    alldata_map = d[0]["craftData"]
-    map = d[alldata_map]
-    for i in map:
-        clanmap = d[map[i]]
-        clanmap.sort(reverse=False)
-        for datamap in clanmap:
-            data = d[datamap]
-            time = d[data["created_at"]][1][:10]
-            if time not in realdata:
-                realdata[time] = {}
-            realdata[time][i] = [d[data["win_rate"]],d[data["play_rate"]]]
-    with open(join(MOUDULE_PATH,'data','ratings.json'),'w',encoding="utf-8") as f:
-        json.dump(realdata, f,indent=4,ensure_ascii=False)
-    print("Ratings更新完毕")
-
 
 async def master_update():
-    tasks = [rating_update(),deck_update()]
+    tasks = [deck_update()]
     await asyncio.wait(tasks)
 
