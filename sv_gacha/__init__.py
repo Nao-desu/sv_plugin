95f04fd23ccbace0c5bbabf7cf542fb216d4a151
夏莫 <2838596569@qq.com>
2024-06-10 00:09:53 +0800

    md模板不能上传空字符串

--- sv_gacha/__init__.py
+++ sv_gacha/__init__.py
@@ -1,6 +1,6 @@
 from hoshino import Service
 from hoshino.util import DailyNumberLimiter
-from ..info import check_set
+from ..info import check_set,card_set
 from .gacha import gachaing
 from .img_gen import draw_result_1,draw_result_2
 from ..config import max_400,max_coin
@@ -28,15 +28,15 @@ async def gacha1(bot,ev):
             await bot.send(ev,f'今天已经抽过{max_coin}金币啦,请明天再来',at_sender = True)
             return
         text:str = ev.message.extract_plain_text().strip()
-        card_set:int = check_set(text)
-        if not card_set:
+        card_set_gacha:int = check_set(text)
+        if not card_set_gacha:
             await bot.send(ev,'不存在此卡包！',at_sender= True)
             return
-        leadercard,card,_ = await gachaing(card_set,1,False)
+        leadercard,card,_ = await gachaing(card_set_gacha,1,False)
         url,size = await draw_result_1(leadercard,card)
         clmt.increase(f'{uid}',100)
         button = [{"buttons":[button_gen(False,'单抽','sv抽卡'),button_gen(False,'十连','sv十连'),button_gen(False,'一井','sv井')]}]
-        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,'',''],button)
+        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,'下次运气会更好！',f'抽取卡包:{card_set[card_set_gacha]}'],button)
         await bot.send(ev,msg)
     except Exception as e:
         await bot.send(ev,f'发送失败：{e}')
@@ -50,11 +50,11 @@ async def gacha10(bot,ev):
             await bot.send(ev,f'今天已经抽过{max_coin}金币啦,请明天再来',at_sender = True)
             return
         text:str = ev.message.extract_plain_text().strip()
-        card_set:int = check_set(text)
-        if not card_set:
+        card_set_gacha:int = check_set(text)
+        if not card_set_gacha:
             await bot.send(ev,'不存在此卡包！',at_sender= True)
             return
-        leadercard,card,result = await gachaing(card_set,10,False)
+        leadercard,card,result = await gachaing(card_set_gacha,10,False)
         msg = '进行了10次抽卡,获得:'
         if leadercard:
             msg += f'  \r异画x{len(leadercard)}'
@@ -69,7 +69,7 @@ async def gacha10(bot,ev):
         msg += f'  \r获得以太{result[1]*1000+result[2]*250+result[3]*50+result[4]*10}'
         url,size = await draw_result_2(leadercard,card,False)
         button = [{"buttons":[button_gen(False,'单抽','sv抽卡'),button_gen(False,'十连','sv十连'),button_gen(False,'一井','sv井')]}]
-        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,msg,''],button)
+        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,msg,'f'抽取卡包:{card_set[card_set_gacha]}'],button)
         await bot.send(ev,msg)
         clmt.increase(f'{uid}',1000)
     except Exception as e:
@@ -84,12 +84,12 @@ async def gacha400(bot,ev):
             await bot.send(ev,f'今天已经抽过{max_400}井啦,请明天再来',at_sender = True)
             return
         text:str = ev.message.extract_plain_text().strip()
-        card_set:int = check_set(text)
-        if not card_set:
+        card_set_gacha:int = check_set(text)
+        if not card_set_gacha:
             await bot.send(ev,'不存在此卡包！',at_sender= True)
             return
-        roll_time = 400 if  not jlmt.get_num(f'{uid}{card_set}') else 300
-        leadercard,card,result = await gachaing(card_set,roll_time,True)
+        roll_time = 400 if  not jlmt.get_num(f'{uid}{card_set_gacha}') else 300
+        leadercard,card,result = await gachaing(card_set_gacha,roll_time,True)
         msg = f'进行了{roll_time}次抽卡,获得:'
         if leadercard:
             msg += f'  \r异画x{len(leadercard)}'
@@ -104,10 +104,10 @@ async def gacha400(bot,ev):
         msg += f'  \r获得以太{result[1]*1000+result[2]*250+result[3]*50+result[4]*10}'
         url,size = await draw_result_2(leadercard,card,True)
         button = [{"buttons":[button_gen(False,'单抽','sv抽卡'),button_gen(False,'十连','sv十连'),button_gen(False,'一井','sv井')]}]
-        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,msg,''],button)
+        msg = MD_gen(['抽卡结果',f'img#{size[0]}px #{size[1]}px',url,msg,'f'抽取卡包:{card_set[card_set_gacha]}'],button)
         await bot.send(ev,msg)
         tlmt.increase(f'{uid}')
-        jlmt.increase(f'{uid}{card_set}')
+        jlmt.increase(f'{uid}{card_set_gacha}')
     except Exception as e:
         await bot.send(ev,f'发送失败：{e}')
         traceback.print_exc()
